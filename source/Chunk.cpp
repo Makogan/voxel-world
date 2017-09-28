246c9977789785e210a01b8a2534595cee710356
camilo.talero <camilo.talero@ucalgary.com>
2017-09-28 14:23:16 -0600

    Fixed cunks

--- source/Chunk.cpp
+++ source/Chunk.cpp
@@ -27,11 +27,15 @@
 //TODO: comment this section
 Chunk::Chunk()
 {
-    for(int i=0; i<16*16*16; i++)
+    int side = 16;
+    for(int i=0; i<side*side*side; i++)
     {
         chunk_cubes[i] = new Cube("Assets/Objs/cube.obj", "Assets/Textures/Cube-map.png",
-            2.f*vec3((i/(16*16), (i/16) % 16, i % 16)));
-        cout << "(" << i/(16*16) << ", " << (i/16) %16 << ", " << i%16 << ")" << endl;
+            2.f*vec3(i/(side*side), (i/side) % side, i % side));
+       // vec3 temp = chunk_cubes[i]->position;
+     /*   cout << temp << endl;
+        cout << "(" << i/(side*side) << ", " << (i/side) %side << ", " << i%side << ")" << endl;
+        cout << "wtf" << endl;*/
     }
 }
 
