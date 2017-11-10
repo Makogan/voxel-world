6241b6e92d6e742e2d85400f7e4661dba45536a0
camilo.talero <camilo.talero@ucalgary.com>
2017-11-10 14:50:30 -0700

    Shore lines added

--- source/Rendering/Shaders/FragmentShader.glsl
+++ source/Rendering/Shaders/FragmentShader.glsl
@@ -85,10 +85,10 @@ void main()
   double y = floor(gl_FragCoord.y/4)/10.f;
 
   double total=0;
-  double frequency=1;
-  double amplitude=1;
+  double frequency=0.05;
+  double amplitude=10;
   double maxValue=0;
-  double persistence = 0.9;
+  double persistence = 0.5;
   for(uint i=0; i<5; i++)
   {
     total += perlin_noise(x*frequency, y*frequency)*amplitude;
@@ -98,5 +98,11 @@ void main()
     frequency *= 2;
   }
   double p=total/maxValue;
+  if(abs(p)<0.01)
+    p+=perlin_noise(x*1.5,y*1.5);
+  if(p>=0)
+    p=1;
+  else
+    p=-1;
   outColor = (vec4(p,p,p,1));
 }
